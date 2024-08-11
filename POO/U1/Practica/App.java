f266a8ac2392412c4cea2014142e8aa090c963c2
AxelV <axelkevinagustinrojas@gmail.com>
2024-08-10 21:43:57 -0300

    Cambio de directorios, scaffolding de Practica

--- POO/U1/Practica/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Serie serie = new Serie("imagen", "breaking bad", "Estrena todos los lunes");
-        Categoria categoria = new Categoria("Accion");
-    }
-}
--- POO/U1/Practica/EJ3/main.py
+++
@@ -1,9 +0,0 @@
-from Serie import Serie
-from Categoria import Categoria
-
-serie = Serie("imagen", "breaking bad", "Estrena todos los jueves")
-serie1 = Serie("imagen", "game of thrones", "Estrena todos los jueves")
-serie2 = Serie("imagen", "Morty", "Estrena todos los jueves")
-
-categoria = Categoria ("Estreno semanal")
-categoria.agregar_serie(Serie)
